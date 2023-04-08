8370d539042ffb011f7726811968a94f9b0dbaf4
Matthew <863968+mattbis@users.noreply.github.com>
2023-04-08 19:31:16 +0100

    Update make_a_patch.sh

--- sh/make_a_patch.sh
+++ sh/make_a_patch.sh
@@ -1,1 +1,6 @@
-
+mkdir b/patch
+### patching does this 
+diff -patch etc
+# change teh src project vxproject thing to new targets
+# add some new stuff to make it work that i will figure out but got confused about
+# apply source changes
