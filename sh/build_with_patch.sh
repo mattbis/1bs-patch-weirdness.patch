f402ae889df27fbe2b288abefec931eac3d371fb
Matthew <863968+mattbis@users.noreply.github.com>
2023-04-08 18:48:09 +0100

    f: think about the build with patch

--- sh/build_with_patch.sh
+++ sh/build_with_patch.sh
@@ -1,1 +1,13 @@
+mkdir var/src
+mkdir var/build
+mkdir var/release
 
+# copy orig 
+# apply diff to orig
+
+# in the build files probably
+
+# transform src with xsl
+# set env vars for new structure
+# call builder link, build
+# release post
